516f4ed3f50b1a9f7f6f5b6bafe73dc085844898
RiightX <82491290+RiightX@users.noreply.github.com>
2022-05-18 23:06:17 -0500

    Use cleaned up example

--- whw.lua
+++ whw.lua
@@ -1,3 +1,22 @@
-local name = UnitName("player")
+SLASH_HELLO1 = "/helloworld"
+SLASH_HELLO2 = "/msg"
 
-message("Hello, " .. name .. "!")
+local function showGreeting(name)
+    local greeting = "Hello, " .. name .. "!"
+
+    message(greeting)
+end
+
+local function HelloWorldHandler(name)
+    local nameExists = string.len(name) > 0
+
+    if(nameExists) then
+        showGreeting(name)
+    else
+        local playerName = UnitName("player")
+
+        showGreeting(playerName)
+    end
+end
+
+SlashCmdList["HELLO"] = HelloWorldHandler
